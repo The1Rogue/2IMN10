b3040c2b48662ea126f8f7c01242787825d1227e
woutheijne <w.h.m.heijne@student.tue.nl>
2025-10-19 16:06:48 +0200

    Added health checks for servers

--- load_balancer/low_conn.py
+++ load_balancer/low_conn.py
@@ -8,9 +8,13 @@ HOST = os.getenv("HOST", "wc_balancer")
 PORT = int(os.getenv("PORT", "18861"))
 
 REMOTES = os.getenv("REMOTE", "wc_server").split(";")
-print(REMOTES)
-print(len(REMOTES))
+# print(REMOTES)
+# print(len(REMOTES))
 c = [0 for _ in REMOTES] #connection count
+
+HEALTH_CHECK_PORT = int(os.getenv("CHECK_PORT", "18861"))
+HEALTH_CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "1"))
+HEALTH = [True for _ in REMOTES]
 
 async def proxy(read, write):
 	while True:
@@ -23,16 +27,44 @@ async def proxy(read, write):
 	write.close()
 
 
+async def health_check():
 
+	while True:
+		await asyncio.sleep(HEALTH_CHECK_INTERVAL)
+		for i, s in enumerate(REMOTES):
+			is_healthy = False
+			try:
+				reader, writer = await asyncio.wait_for(asyncio.open_connection(s, HEALTH_CHECK_PORT), timeout=0.5)
+				writer.close()
+				await writer.wait_closed()
+				is_healthy = True
+			except:
+				is_healthy = False
+
+			if is_healthy and not HEALTH[i]:
+				print(f"Server {s} ready for connections")
+			elif not is_healthy and HEALTH[i]:
+				print(f"Server {s} not ready for connections")
+			HEALTH[i] = is_healthy
+          
+             
 async def handle_conn(source_read, source_write):
-	i = min(range(len(c)), key=c.__getitem__)
+	healthy = [i for i, h in enumerate(HEALTH) if h]
+	if not healthy:
+		print("No healthy servers") # What to do?
+
+	i = min(healthy, key=c.__getitem__)
+
 	r = REMOTES[i]
-	print(r)
+	# print(r)
 	c[i] += 1
 
-	target_read, target_write = await asyncio.open_connection(r, PORT)
-	await asyncio.wait([proxy(source_read, target_write), proxy(target_read, source_write)])
-
+	try:
+		target_read, target_write = await asyncio.open_connection(r, PORT)
+		await asyncio.wait([proxy(source_read, target_write), proxy(target_read, source_write)])
+	except:
+		print(f"Server {r} failed during execution")
+		HEALTH[i] = False
 	c[i] -= 1
 
 
@@ -40,6 +72,8 @@ async def handle_conn(source_read, source_write):
 loop = asyncio.get_event_loop()
 coroutine = asyncio.start_server(handle_conn, HOST, PORT, loop=loop)
 server = loop.run_until_complete(coroutine)
+
+health_task = loop.create_task(health_check())
 
 try:
 	loop.run_forever()
--- load_balancer/round_robin.py
+++ load_balancer/round_robin.py
@@ -12,6 +12,10 @@ print(REMOTES)
 print(len(REMOTES))
 i = 0
 
+HEALTH_CHECK_PORT = int(os.getenv("CHECK_PORT", "18861"))
+HEALTH_CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "1"))
+HEALTH = [True for _ in REMOTES]
+
 async def proxy(read, write):
 	while True:
 		data = await read.read(1024)
@@ -22,14 +26,41 @@ async def proxy(read, write):
 
 	write.close()
 
+async def health_check():
 
+	while True:
+		await asyncio.sleep(HEALTH_CHECK_INTERVAL)
+		for i, s in enumerate(REMOTES):
+			is_healthy = False
+			try:
+				reader, writer = await asyncio.wait_for(asyncio.open_connection(s, HEALTH_CHECK_PORT), timeout=0.5)
+				writer.close()
+				await writer.wait_closed()
+				is_healthy = True
+			except:
+				is_healthy = False
+
+			if is_healthy and not HEALTH[i]:
+				print(f"Server {s} ready for connections")
+			elif not is_healthy and HEALTH[i]:
+				print(f"Server {s} not ready for connections")
+			HEALTH[i] = is_healthy
 
 async def handle_conn(source_read, source_write):
 	global i
-	r = REMOTES[i]
+
+	ti = i
+	while not HEALTH[ti]:
+		ti = (ti + 1) % len(REMOTES)
+		if ti == i:
+			print("No healthy servers") # What to do?
+			break
+	
+
+	r = REMOTES[ti]
 	print(r)
-	i = (i + 1) % len(REMOTES)
-	print(i)
+	i = (ti + 1) % len(REMOTES)
+	# print(i)
 	target_read, target_write = await asyncio.open_connection(r, PORT)
 
 	await asyncio.wait([proxy(source_read, target_write), proxy(target_read, source_write)])
@@ -39,6 +70,8 @@ async def handle_conn(source_read, source_write):
 loop = asyncio.get_event_loop()
 coroutine = asyncio.start_server(handle_conn, HOST, PORT, loop=loop)
 server = loop.run_until_complete(coroutine)
+
+health_task = loop.create_task(health_check())
 
 try:
 	loop.run_forever()
